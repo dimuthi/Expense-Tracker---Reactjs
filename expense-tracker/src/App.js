fafe6eaa84cbc7ab5c040f0c6d89af6fb1efb261
dimuthi tharaka <dimuthi@gmail.com>
2021-12-27 16:37:08 +0530

    created expenses component

--- expense-tracker/src/App.js
+++ expense-tracker/src/App.js
@@ -1,9 +1,9 @@
 import "./App.css";
 import "./Components/ExpenseItem";
-import ExpenseItem from "./Components/ExpenseItem";
+import Expenses from "./Components/Expenses";
 
 function App() {
-  const items = [
+  const expenses = [
     { title: "Music", amount: 2000, date: new Date(2021, 1, 1) },
     { title: "Shopping", amount: 5000, date: new Date(2021, 1, 1) },
     { title: "Food", amount: 3000, date: new Date(2021, 1, 1) },
@@ -12,26 +12,7 @@ function App() {
   return (
     <div>
       <h1 align="center">Expenses Tracker</h1>
-      <ExpenseItem
-        title={items[0].title}
-        amount={items[0].amount}
-        date={items[0].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={items[1].title}
-        amount={items[1].amount}
-        date={items[1].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={items[2].title}
-        amount={items[2].amount}
-        date={items[2].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={items[3].title}
-        amount={items[3].amount}
-        date={items[3].date}
-      ></ExpenseItem>
+      <Expenses items={expenses}></Expenses>
     </div>
   );
 }
