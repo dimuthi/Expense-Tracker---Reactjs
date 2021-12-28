8e7ac74f9ed4d20671bb76242bb8ad8cdb07ad86
dimuthi tharaka <dimuthi@gmail.com>
2021-12-28 11:30:04 +0530

    added card component

--- expense-tracker/src/Components/ExpenseItem.js
+++ expense-tracker/src/Components/ExpenseItem.js
@@ -1,18 +1,21 @@
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
+import Card from "./Card";
 
 function ExpenseItem(props) {
   //   const expenseDate = new Date(2021, 1, 1);
   //   const expenseTitle = "car expense";
   //   const expenseAmount = 2000;
   return (
-    <div className="expense-item">
-      <div><ExpenseDate date={props.date}></ExpenseDate></div>
+    <Card className="expense-item">
+      <div>
+        <ExpenseDate date={props.date}></ExpenseDate>
+      </div>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">LKR{props.amount}</div>
       </div>
-    </div>
+    </Card>
   );
 }
 
